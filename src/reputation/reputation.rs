c7899ead9ab7d27f2ab42eae98707a27120334ce
ChainRepAI <chainrepai@gmail.com>
2025-01-25 18:35:31 +0000

    Add From account transactions to Reputation item implementation

--- src/reputation/reputation.rs
+++ src/reputation/reputation.rs
@@ -41,3 +41,43 @@ impl From<&u64> for ReputationItem {
         Self { level, reasoning }
     }
 }
+
+/// Good transaction volume indicates high reputation
+/// Too high volume indicates automation and hence, lower reputation
+/// No volume indicates no reputation
+impl From<&Vec<RpcConfirmedTransactionStatusWithSignature>> for ReputationItem {
+    fn from(transaction_history: &Vec<RpcConfirmedTransactionStatusWithSignature>) -> Self {
+        let num_hours = transaction_history
+            .first()
+            .and_then(|first_tx| first_tx.block_time)
+            .zip(
+                transaction_history
+                    .last()
+                    .and_then(|last_tx| last_tx.block_time),
+            )
+            .map(|(first_tx_time, last_tx_time)| (first_tx_time - last_tx_time).abs() / 3600)
+            .unwrap_or(0);
+        let tx_per_hour = transaction_history.len() as i64 / num_hours;
+
+        let (level, reasoning) = match tx_per_hour {
+            v if v == 0 => (
+                ReputationLevel::None,
+                vec!["No transaction volume".to_string()],
+            ),
+            v if v < 10 => (
+                ReputationLevel::Medium,
+                vec!["Low transaction volume".to_string()],
+            ),
+            v if v < 100 => (
+                ReputationLevel::High,
+                vec!["Reasonable level of transaction volume".to_string()],
+            ),
+            _ => (
+                ReputationLevel::Low,
+                vec!["Transaction volume too high".to_string()],
+            ),
+        };
+
+        Self { level, reasoning }
+    }
+}
