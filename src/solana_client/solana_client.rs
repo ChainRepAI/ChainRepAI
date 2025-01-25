23c612d6e9555b944d8d4356b68f4e60242d19b5
ChainRepAI <chainrepai@gmail.com>
2025-01-25 15:33:33 +0000

    Add required dependencies to solana client

--- src/solana_client/solana_client.rs
+++ src/solana_client/solana_client.rs
@@ -1,4 +1,11 @@
-use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_client::{
+    client_error::ClientError,
+    nonblocking::rpc_client::RpcClient,
+    rpc_request::TokenAccountsFilter,
+    rpc_response::{RpcConfirmedTransactionStatusWithSignature, RpcKeyedAccount},
+};
+use solana_sdk::{account::Account, pubkey::Pubkey};
+use std::env;
 
 pub struct SolanaClient {
     client: RpcClient,
