b2f1a923351197bd2f860b491b5b4c27fca1c3f8
ChainRepAI <chainrepai@gmail.com>
2025-01-25 15:38:05 +0000

    fmt and remove unused dependency

--- src/solana_client/solana_client.rs
+++ src/solana_client/solana_client.rs
@@ -1,7 +1,6 @@
 use solana_client::{
     client_error::ClientError,
     nonblocking::rpc_client::RpcClient,
-    rpc_request::TokenAccountsFilter,
     rpc_response::{RpcConfirmedTransactionStatusWithSignature, RpcKeyedAccount},
 };
 use solana_sdk::{account::Account, pubkey::Pubkey};
@@ -10,15 +9,18 @@ use std::env;
 pub struct SolanaClient {
     client: RpcClient,
 }
+
 impl SolanaClient {
     pub fn new() -> Self {
         Self {
             client: RpcClient::new(env::var("RPC_URL").expect("rpc url should be set")),
         }
     }
+
     pub async fn get_account_balance(&self, pub_key: &Pubkey) -> Result<u64, ClientError> {
         self.client.get_balance(pub_key).await
     }
+
     pub async fn get_account_info(&self, pub_key: &Pubkey) -> Result<Account, ClientError> {
         self.client.get_account(pub_key).await
     }
