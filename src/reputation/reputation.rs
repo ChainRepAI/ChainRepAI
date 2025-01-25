2e8ce9a163c38d6ae24bdb54ba3de2b6e63a9a4f
ChainRepAI <chainrepai@gmail.com>
2025-01-25 18:34:37 +0000

    Add From impl from account balance to repuation item

--- src/reputation/reputation.rs
+++ src/reputation/reputation.rs
@@ -17,3 +17,27 @@ struct ReputationItem {
     level: ReputationLevel,
     reasoning: Vec<String>,
 }
+impl From<&u64> for ReputationItem {
+    fn from(bal: &u64) -> Self {
+        let (level, reasoning) = match *bal {
+            b if b < 1 => (
+                ReputationLevel::None,
+                vec!["Balance < 1 Solana".to_string()],
+            ),
+            b if b < 10 => (
+                ReputationLevel::Medium,
+                vec!["Balance between 1 and 10 Solana".to_string()],
+            ),
+            b if b < 100 => (
+                ReputationLevel::Medium,
+                vec!["Balance between 10 and 100 Solana".to_string()],
+            ),
+            _ => (
+                ReputationLevel::High,
+                vec!["Balance >= 100 Solana".to_string()],
+            ),
+        };
+
+        Self { level, reasoning }
+    }
+}
