711d24379dd7445b0c78e2f2c269003fd3035a10
ChainRepAI <chainrepai@gmail.com>
2025-01-26 00:53:37 +0000

    Add wallet reputation endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 use serde::Serialize;
 use ChainRepAI::{reputation::reputation::{Reputation, ReputationItem, ReputationLevel}, solana_client::solana_client::SolanaClient, wallet::wallet::Wallet};
 
@@ -13,9 +15,24 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[get("/wallet_reputation/{wallet_addr}")]
+async fn wallet_reputation(wallet_addr: web::Path<String>) -> impl Responder {
+    dotenv().ok();
+    let solana_client = SolanaClient::new();
+    let wallet = Wallet::new(wallet_addr.as_str(), &solana_client).await;
+    let mut reputation = Reputation::new();
+    reputation.calc_reputation(&wallet);
+    let reputation_response = ReputationResponse {
+        wallet_addr: wallet_addr.to_string(),
+        reputation_items: reputation.items,
+        average_reputation: ReputationLevel::Medium,
+    };
+    HttpResponse::Ok().json(reputation_response)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health_check))
+    HttpServer::new(|| App::new().service(health_check).service(wallet_reputation))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
