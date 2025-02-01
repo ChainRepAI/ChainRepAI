0dc575a97d476075b5e6997c542cc5c8cb1579db
ChainRepAI <chainrepai@gmail.com>
2025-02-01 14:58:46 +0000

    Add PulsarClient method to create producer

--- src/pulsar/pulsar.rs
+++ src/pulsar/pulsar.rs
@@ -1,4 +1,4 @@
-use pulsar::{Producer, Pulsar, TokioExecutor};
+use pulsar::{producer, proto, Producer, Pulsar, TokioExecutor};
 use uuid::Uuid;
 
 const PULSAR_ADDR: &str = "pulsar://localhost:6650";
@@ -14,6 +14,30 @@ impl PulsarClient {
                 .build()
                 .await
                 .expect("Should be able to create new pulsar client builder"),
+        }
+    }
+
+    pub async fn create_producer(&self, topic: &str) -> PulsarProducer {
+        let id = Uuid::new_v4();
+
+        PulsarProducer {
+            id,
+            internal_producer: self
+                .internal_client
+                .producer()
+                .with_topic(topic)
+                .with_name("PRODUCER_".to_owned() + &id.to_string())
+                .with_options(producer::ProducerOptions {
+                    schema: Some(proto::Schema {
+                        r#type: proto::schema::Type::String as i32,
+                        ..Default::default()
+                    }),
+
+                    ..Default::default()
+                })
+                .build()
+                .await
+                .expect("Should be able to create producer"),
         }
     }
 }
