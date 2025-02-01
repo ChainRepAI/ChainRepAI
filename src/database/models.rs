469078dc06a94de42768cdcfa2b1606032ac27c3
ChainRepAI <chainrepai@gmail.com>
2025-02-01 14:42:16 +0000

    Implement seralize and deseralize for RatingClassification

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,6 +1,11 @@
+use std::io::Write;
+
 use chrono::NaiveDateTime;
+use diesel::deserialize::{self, FromSql};
 use diesel::expression::AsExpression;
+use diesel::pg::{Pg, PgValue};
 use diesel::prelude::{Insertable, Queryable};
+use diesel::serialize::{self, IsNull, Output, ToSql};
 use serde::Serialize;
 use uuid::Uuid;
 
@@ -16,6 +21,42 @@ pub enum RatingClassification {
     CCC,
     CC,
     C,
+}
+
+impl ToSql<crate::database::schema::sql_types::RatingClassification, Pg> for RatingClassification {
+    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
+        match *self {
+            RatingClassification::AAA => out.write_all(b"aaa")?,
+            RatingClassification::AA => out.write_all(b"aa")?,
+            RatingClassification::A => out.write_all(b"a")?,
+            RatingClassification::BBB => out.write_all(b"bbb")?,
+            RatingClassification::BB => out.write_all(b"bb")?,
+            RatingClassification::B => out.write_all(b"b")?,
+            RatingClassification::CCC => out.write_all(b"ccc")?,
+            RatingClassification::CC => out.write_all(b"cc")?,
+            RatingClassification::C => out.write_all(b"c")?,
+        }
+        Ok(IsNull::No)
+    }
+}
+
+impl FromSql<crate::database::schema::sql_types::RatingClassification, Pg>
+    for RatingClassification
+{
+    fn from_sql(bytes: PgValue) -> deserialize::Result<Self> {
+        match bytes.as_bytes() {
+            b"aaa" => Ok(RatingClassification::AAA),
+            b"aa" => Ok(RatingClassification::AA),
+            b"a" => Ok(RatingClassification::A),
+            b"bbb" => Ok(RatingClassification::BBB),
+            b"bb" => Ok(RatingClassification::BB),
+            b"b" => Ok(RatingClassification::B),
+            b"ccc" => Ok(RatingClassification::CCC),
+            b"cc" => Ok(RatingClassification::CC),
+            b"c" => Ok(RatingClassification::C),
+            _ => Err("Unrecognized enum variant".into()),
+        }
+    }
 }
 
 impl From<i32> for RatingClassification {
