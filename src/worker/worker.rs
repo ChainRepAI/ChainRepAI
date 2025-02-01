a2408b9a6fdd18bb53de9c4b035ee99dfc96b78a
ChainRepAI <chainrepai@gmail.com>
2025-02-01 15:56:08 +0000

    Impl do_work for WalletReportWorker

--- src/worker/worker.rs
+++ src/worker/worker.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use futures::TryStreamExt;
 use pulsar::SubType;
 
 use crate::{
@@ -12,9 +14,9 @@ const SUB: &str = "wallet-reputation-sub";
 const SUB_TYPE: SubType = SubType::Shared;
 
 pub struct WalletReportWorker {
-    database: Database,
-    solana_client: SolanaClient,
-    openai_client: OpenAIClient,
+    pub database: Database,
+    pub solana_client: SolanaClient,
+    pub openai_client: OpenAIClient,
     job_consumer: PulsarConsumer,
 }
 
@@ -29,5 +31,36 @@ impl WalletReportWorker {
                 .create_consumer(vec![WALLET_REPUTATION_TOPIC], SUB_TYPE, SUB)
                 .await,
         }
+    }
+
+    pub async fn do_work(&mut self) -> Result<()> {
+        while let Some(msg) = self
+            .job_consumer
+            .internal_consumer
+            .try_next()
+            .await
+            .expect("Should be able to wait for new message.")
+        {
+            let wallet_reputation_job = match msg.deserialize() {
+                Ok(data) => data,
+                Err(e) => {
+                    println!("Couldn't deseralize job, error: {:?}.", e);
+                    continue;
+                }
+            };
+
+            match wallet_reputation_job.do_job(self).await {
+                Ok(_) => {
+                    self.job_consumer.ack(&msg).await;
+                    println!("Job processed successfully");
+                }
+                Err(_) => {
+                    self.job_consumer.nack(&msg).await;
+                    println!("Couldn't process job successfully");
+                }
+            }
+        }
+
+        Ok(())
     }
 }
