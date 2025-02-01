832a8bfa36feafa020e6129a46d9d26910eb5261
ChainRepAI <chainrepai@gmail.com>
2025-02-01 15:20:26 +0000

    Add job_consumer to WalletReportWorker

--- src/worker/worker.rs
+++ src/worker/worker.rs
@@ -1,17 +1,33 @@
-use crate::{database::postgres::Database, openai_client::openai_client::OpenAIClient, solana_client::solana_client::SolanaClient};
+use pulsar::SubType;
+
+use crate::{
+    database::postgres::Database,
+    openai_client::openai_client::OpenAIClient,
+    pulsar::pulsar::{PulsarClient, PulsarConsumer},
+    solana_client::solana_client::SolanaClient,
+};
+
+pub const WALLET_REPUTATION_TOPIC: &str = "non-persistent://public/default/wallet-reputation";
+const SUB: &str = "wallet-reputation-sub";
+const SUB_TYPE: SubType = SubType::Shared;
 
 pub struct WalletReportWorker {
     database: Database,
     solana_client: SolanaClient,
     openai_client: OpenAIClient,
+    job_consumer: PulsarConsumer,
 }
 
 impl WalletReportWorker {
-    pub fn new() -> Self {
+    pub async fn new() -> Self {
+        let pulsar_client = PulsarClient::new().await;
         Self {
             database: Database::connect().expect("Should be able to connect to db"),
             solana_client: SolanaClient::new(),
             openai_client: OpenAIClient::new(),
+            job_consumer: pulsar_client
+                .create_consumer(vec![WALLET_REPUTATION_TOPIC], SUB_TYPE, SUB)
+                .await,
         }
     }
 }
