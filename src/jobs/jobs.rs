4609597bccb895f270d550f4ca5a5ea855168caa
ChainRepAI <chainrepai@gmail.com>
2025-02-01 15:04:39 +0000

    Impl seralize and deseralze for wallet report job

--- src/jobs/jobs.rs
+++ src/jobs/jobs.rs
@@ -1,7 +1,26 @@
+use pulsar::{producer, DeserializeMessage, Error as PulsarError, SerializeMessage};
 use serde::{Deserialize, Serialize};
 
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WalletReportJob {
     wallet_addr: String,
+}
+
+impl SerializeMessage for WalletReportJob {
+    fn serialize_message(input: Self) -> Result<producer::Message, PulsarError> {
+        let payload = serde_json::to_vec(&input).map_err(|e| PulsarError::Custom(e.to_string()))?;
+        Ok(producer::Message {
+            payload,
+            ..Default::default()
+        })
+    }
+}
+
+impl DeserializeMessage for WalletReportJob {
+    type Output = Result<WalletReportJob, serde_json::Error>;
+
+    fn deserialize_message(payload: &pulsar::Payload) -> Self::Output {
+        serde_json::from_slice(&payload.data)
+    }
 }
