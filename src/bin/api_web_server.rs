ae173fd9ef7583e87ebe528a95aeaba26cb3de6e
ChainRepAI <chainrepai@gmail.com>
2025-02-01 19:19:45 +0000

    Add get report classification endpoint

--- src/bin/api_web_server.rs
+++ src/bin/api_web_server.rs
@@ -3,7 +3,10 @@ use anyhow::Result;
 use dotenv::dotenv;
 use uuid::Uuid;
 use SolAnalystAI::{
-    database::{models::WalletReport, postgres::Database},
+    database::{
+        models::{RatingClassification, WalletReport},
+        postgres::Database,
+    },
     jobs::jobs::WalletReportJob,
     pulsar::pulsar::PulsarClient,
     worker::worker::WALLET_REPUTATION_TOPIC,
@@ -14,14 +17,26 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+fn get_wallet_report_classification(report_id: Uuid) -> Result<RatingClassification> {
+    let mut database = Database::connect()?;
+    Ok(database.get_wallet_report_classification(report_id)?)
+}
+
 fn get_wallet_report(report_id: Uuid) -> Result<WalletReport> {
     let mut database = Database::connect()?;
     Ok(database.get_wallet_report(report_id)?)
 }
 
+#[get("/get_wallet_report_classification/{report_id}")]
+async fn get_wallet_report_classification_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    match get_wallet_report_classification(*report_id) {
+        Ok(classification) => HttpResponse::Ok().json(classification),
+        Err(_) => HttpResponse::InternalServerError().json("Internal Server Error"),
+    }
+}
+
 #[get("/get_wallet_report/{report_id}")]
 async fn get_wallet_report_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
-    dotenv().ok();
     match get_wallet_report(*report_id) {
         Ok(wallet_report) => HttpResponse::Ok().json(wallet_report),
         Err(_) => HttpResponse::InternalServerError().json("Internal Server Error"),
@@ -30,8 +45,6 @@ async fn get_wallet_report_endpoint(report_id: web::Path<Uuid>) -> impl Responde
 
 #[post("/start_wallet_report/{wallet_addr}")]
 async fn start_wallet_report_endpoint(wallet_addr: web::Path<String>) -> impl Responder {
-    dotenv().ok();
-
     let pulsar_client = PulsarClient::new().await;
     let mut pulsar_producer = pulsar_client.create_producer(WALLET_REPUTATION_TOPIC).await;
     let id = Uuid::new_v4();
@@ -48,11 +61,13 @@ async fn start_wallet_report_endpoint(wallet_addr: web::Path<String>) -> impl Re
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     HttpServer::new(|| {
         App::new()
             .service(health_check)
             .service(get_wallet_report_endpoint)
             .service(start_wallet_report_endpoint)
+            .service(get_wallet_report_classification_endpoint)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
