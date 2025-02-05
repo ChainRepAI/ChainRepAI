82fb4066021f4832e3cd9768374838b3c7520651
ChainRepAI <chainrepai@gmail.com>
2025-02-05 21:07:47 +0000

    Add logging to endpoints

--- src/bin/api_web_server.rs
+++ src/bin/api_web_server.rs
@@ -2,6 +2,7 @@ use actix_web::{delete, get, post, web, App, HttpResponse, HttpServer, Responder
 use anyhow::Result;
 use chrono::NaiveDateTime;
 use dotenv::dotenv;
+use log::{error, info};
 use uuid::Uuid;
 use SolAnalystAI::{
     case_report::case_report::CaseReport,
@@ -69,33 +70,77 @@ fn get_wallet_report(report_id: Uuid) -> Result<WalletReport> {
 
 #[delete("/delete_user/{api_key}")]
 async fn delete_user_endpoint(api_key: web::Path<String>) -> impl Responder {
+    info!("Received delete user request for API key: {}", api_key);
     match delete_user(api_key.as_str()) {
-        Ok(_) => HttpResponse::Ok().json("Successfully deleted user"),
-        Err(_) => HttpResponse::InternalServerError().json("Unable to process request"),
+        Ok(_) => {
+            info!("Successfully deleted user with API key: {}", api_key);
+            HttpResponse::Ok().json("Successfully deleted user")
+        }
+        Err(err) => {
+            error!("Failed to delete user with API key {}: {:?}", api_key, err);
+            HttpResponse::InternalServerError().json("Unable to process request")
+        }
     }
 }
 
 #[delete("/delete_wallet_report/{report_id}")]
 async fn delete_wallet_report_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received delete wallet report request for report ID: {}",
+        report_id
+    );
     match delete_report(*report_id) {
-        Ok(_) => HttpResponse::Ok().json("Successfully deleted wallet report"),
-        Err(_) => HttpResponse::NotFound().json("Report with supplied id doesn't exist"),
+        Ok(_) => {
+            info!("Successfully deleted wallet report with ID: {}", report_id);
+            HttpResponse::Ok().json("Successfully deleted wallet report")
+        }
+        Err(err) => {
+            error!(
+                "Failed to delete wallet report with ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::NotFound().json("Report with supplied id doesn't exist")
+        }
     }
 }
 
 #[get("/get_wallet_report_metrics/{report_id}")]
 async fn get_wallet_report_metrics_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report metrics for report ID: {}",
+        report_id
+    );
     match get_wallet_report_metrics(*report_id) {
-        Ok(wallet_metrics) => HttpResponse::Ok().json(wallet_metrics),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report metrics exists for supplied id"),
+        Ok(wallet_metrics) => {
+            info!(
+                "Successfully retrieved wallet report metrics for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(wallet_metrics)
+        }
+        Err(err) => {
+            error!(
+                "Failed to retrieve wallet report metrics for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::InternalServerError()
+                .json("No wallet report metrics exists for supplied id")
+        }
     }
 }
 
 #[post("/create_user")]
 async fn create_user_endpoint() -> impl Responder {
+    info!("Received request to create user");
     match create_user() {
-        Ok(api_key) => HttpResponse::Ok().json(api_key),
-        Err(_) => HttpResponse::InternalServerError().json("Unable to create user"),
+        Ok(api_key) => {
+            info!("User created successfully with API key: {}", api_key);
+            HttpResponse::Ok().json(api_key)
+        }
+        Err(err) => {
+            error!("Failed to create user: {:?}", err);
+            HttpResponse::InternalServerError().json("Unable to create user")
+        }
     }
 }
 
@@ -103,59 +148,160 @@ async fn create_user_endpoint() -> impl Responder {
 async fn get_wallet_report_creation_count_endpoint(
     wallet_addr: web::Path<String>,
 ) -> impl Responder {
+    info!(
+        "Received wallet report creation count request for wallet address: {}",
+        wallet_addr
+    );
     match get_wallet_report_count(wallet_addr.clone()) {
-        Ok(count) => HttpResponse::Ok().json(count),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report exists for supplied id"),
+        Ok(count) => {
+            info!(
+                "Successfully retrieved creation count for wallet address: {}. Count: {}",
+                wallet_addr, count
+            );
+            HttpResponse::Ok().json(count)
+        }
+        Err(err) => {
+            error!(
+                "Failed to retrieve wallet report creation count for wallet address {}: {:?}",
+                wallet_addr, err
+            );
+            HttpResponse::InternalServerError().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/get_wallet_report_creation_date/{report_id}")]
 async fn get_wallet_report_creation_date_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report creation date for report ID: {}",
+        report_id
+    );
     match get_wallet_report_creation_date(*report_id) {
-        Ok(creation_date) => HttpResponse::Ok().json(creation_date),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report exists for supplied id"),
+        Ok(creation_date) => {
+            info!(
+                "Successfully retrieved creation date for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(creation_date)
+        }
+        Err(err) => {
+            error!(
+                "Failed to get creation date for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::InternalServerError().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/get_wallet_report_case_report/{report_id}")]
 async fn get_wallet_report_case_report_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report case report for report ID: {}",
+        report_id
+    );
     match get_wallet_report_case_report(*report_id) {
-        Ok(case_report) => HttpResponse::Ok().json(case_report),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report exists for supplied id"),
+        Ok(case_report) => {
+            info!(
+                "Successfully retrieved case report for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(case_report)
+        }
+        Err(err) => {
+            error!(
+                "Failed to get case report for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::InternalServerError().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/get_wallet_report_score/{report_id}")]
 async fn get_wallet_report_score_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report score for report ID: {}",
+        report_id
+    );
     match get_wallet_report_score(*report_id) {
-        Ok(score) => HttpResponse::Ok().json(score),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report exists for supplied id"),
+        Ok(score) => {
+            info!(
+                "Successfully retrieved wallet report score for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(score)
+        }
+        Err(err) => {
+            error!(
+                "Failed to retrieve wallet report score for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::InternalServerError().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/get_wallet_report_classification/{report_id}")]
 async fn get_wallet_report_classification_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report classification for report ID: {}",
+        report_id
+    );
     match get_wallet_report_classification(*report_id) {
-        Ok(classification) => HttpResponse::Ok().json(classification),
-        Err(_) => HttpResponse::InternalServerError().json("No wallet report exists for supplied id"),
+        Ok(classification) => {
+            info!(
+                "Successfully retrieved classification for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(classification)
+        }
+        Err(err) => {
+            error!(
+                "Failed to get wallet report classification for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::InternalServerError().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/get_wallet_report/{report_id}")]
 async fn get_wallet_report_endpoint(report_id: web::Path<Uuid>) -> impl Responder {
+    info!(
+        "Received request for wallet report for report ID: {}",
+        report_id
+    );
     match get_wallet_report(*report_id) {
-        Ok(wallet_report) => HttpResponse::Ok().json(wallet_report),
-        Err(_) => HttpResponse::NotFound().json("No wallet report exists for supplied id"),
+        Ok(wallet_report) => {
+            info!(
+                "Successfully retrieved wallet report for report ID: {}",
+                report_id
+            );
+            HttpResponse::Ok().json(wallet_report)
+        }
+        Err(err) => {
+            error!(
+                "Failed to get wallet report for report ID {}: {:?}",
+                report_id, err
+            );
+            HttpResponse::NotFound().json("No wallet report exists for supplied id")
+        }
     }
 }
 
 #[get("/health")]
 async fn health_check() -> impl Responder {
+    info!("Health check requested");
     HttpResponse::Ok()
 }
 
 #[post("/start_wallet_report/{wallet_addr}")]
 async fn start_wallet_report_endpoint(wallet_addr: web::Path<String>) -> impl Responder {
+    info!(
+        "Received request to start wallet report for wallet address: {}",
+        wallet_addr
+    );
     let pulsar_client = PulsarClient::new().await;
     let mut pulsar_producer = pulsar_client.create_producer(WALLET_REPUTATION_TOPIC).await;
     let id = Uuid::new_v4();
@@ -165,8 +311,20 @@ async fn start_wallet_report_endpoint(wallet_addr: web::Path<String>) -> impl Re
     };
 
     match pulsar_producer.enqueue_job(job).await {
-        Ok(_) => HttpResponse::Ok().json(id),
-        Err(_) => HttpResponse::InternalServerError().json("Unable to start wallet report"),
+        Ok(_) => {
+            info!(
+                "Successfully enqueued wallet report job with report ID: {}",
+                id
+            );
+            HttpResponse::Ok().json(id)
+        }
+        Err(err) => {
+            error!(
+                "Failed to enqueue wallet report job for wallet address {}: {:?}",
+                wallet_addr, err
+            );
+            HttpResponse::InternalServerError().json("Unable to start wallet report")
+        }
     }
 }
 
