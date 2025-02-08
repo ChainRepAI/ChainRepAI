bb071feaca7944d3d71c0c8ce61fdd6df0811d4a
ChainRepAI <chainrepai@gmail.com>
2025-02-08 13:30:53 +0000

    Refactor KnownDiscreditedAssociates::new to use KnownAssociates

--- src/reputation/associates.rs
+++ src/reputation/associates.rs
@@ -11,29 +11,11 @@ pub struct KnownDiscreditedAssociates {
 }
 
 impl KnownDiscreditedAssociates {
-    pub fn new(
+    pub fn new_from_associates(
         database: &mut Database,
-        transactions: Vec<EncodedConfirmedTransactionWithStatusMeta>,
+        known_associates: &KnownAssociates,
     ) -> Result<Self> {
-        let keys: Vec<String> = transactions
-            .into_iter()
-            .flat_map(|tx| {
-                tx.transaction
-                    .transaction
-                    .decode()
-                    .map_or_else(Vec::new, |versioned_tx| {
-                        versioned_tx
-                            .message
-                            .static_account_keys()
-                            .iter()
-                            .map(|key| key.to_string())
-                            .collect()
-                    })
-            })
-            .collect();
-
-        let wallets = database.find_discredited_associates(keys)?;
-
+        let wallets = database.find_discredited_associates(&known_associates.wallets)?;
         Ok(Self { wallets })
     }
 }
