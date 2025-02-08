939ae0c3a425c0a885edaa12576a202104b655b2
ChainRepAI <chainrepai@gmail.com>
2025-02-08 12:33:38 +0000

    Implement 'new' method for KnownDiscreditedAssociates

--- src/reputation/associates.rs
+++ src/reputation/associates.rs
@@ -1,6 +1,31 @@
-use crate::database::models::KnownDiscreditedWallet;
+use anyhow::Result;
+use solana_transaction_status::EncodedConfirmedTransactionWithStatusMeta;
 
+use crate::database::{models::KnownDiscreditedWallet, postgres::Database};
 
 pub struct KnownDiscreditedAssociates {
     pub wallets: Vec<KnownDiscreditedWallet>,
+}
+
+impl KnownDiscreditedAssociates {
+    pub fn new(
+        database: &mut Database,
+        transactions: Vec<EncodedConfirmedTransactionWithStatusMeta>,
+    ) -> Result<Self> {
+        let keys: Vec<String> = transactions
+            .into_iter()
+            .flat_map(|tx| {
+                tx.transaction
+                    .transaction
+                    .decode()
+                    .map_or_else(Vec::new, |versioned_tx| {
+                        versioned_tx.message.static_account_keys().iter().map(|key| key.to_string()).collect()
+                    })
+            })
+            .collect();
+
+        let wallets = database.find_discredited_associates(keys)?;
+
+        Ok(Self { wallets })
+    }
 }
