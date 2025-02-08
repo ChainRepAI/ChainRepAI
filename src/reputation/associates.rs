478f0b58715cbaf3d18ca24fd212d596b0f04846
ChainRepAI <chainrepai@gmail.com>
2025-02-08 13:23:48 +0000

    Add 'new' method for KnownCreditedAssociates

--- src/reputation/associates.rs
+++ src/reputation/associates.rs
@@ -41,3 +41,31 @@ impl KnownDiscreditedAssociates {
 pub struct KnownCreditedAssociates {
     pub wallets: Vec<KnownCreditedWallet>,
 }
+
+impl KnownCreditedAssociates {
+    pub fn new(
+        database: &mut Database,
+        transactions: Vec<EncodedConfirmedTransactionWithStatusMeta>,
+    ) -> Result<Self> {
+        let keys: Vec<String> = transactions
+            .into_iter()
+            .flat_map(|tx| {
+                tx.transaction
+                    .transaction
+                    .decode()
+                    .map_or_else(Vec::new, |versioned_tx| {
+                        versioned_tx
+                            .message
+                            .static_account_keys()
+                            .iter()
+                            .map(|key| key.to_string())
+                            .collect()
+                    })
+            })
+            .collect();
+
+        let wallets = database.find_credited_associates(keys)?;
+
+        Ok(Self { wallets })
+    }
+}
